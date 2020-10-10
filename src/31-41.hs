f7ce3453431a2dbab5f1abd0bb8a8143cf29b1f1
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-10-10 17:23:49 +0900

    算数もわからん

---
+++ src/31-41.hs
@@ -0,0 +1,18 @@
+
+--31
+isPrime::Int->Bool
+isPrime n = foldl (\ac x->if ((n `mod` x) /= 0) && ac then True else False) True [2..(n-1)]
+
+--32
+myGCD::Integer -> Integer -> Integer
+myGCD a b 
+    | b == 0 = abs a
+    | otherwise = myGCD b (a `mod` b)
+
+--33
+comprime::Integer->Integer->Bool
+comprime a b = gcd a b == 1
+
+--34
+totient::Integer->Int
+totient n = length $ filter (\x -> gcd n x ==1) [1..n-1]
